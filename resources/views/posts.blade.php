2f47216e5681020210652883753ab70a1963fc2b
tofatoff <mail.mstofa@gmail.com>
2022-03-07 22:53:41 +0700

    move with to model

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,6 +3,15 @@
 @section('contents')
     <h1 class="m-5">{{ $title }}</h1>
 
+    <div class="card mb-3">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+</div>
+
     @foreach ($posts as $post)
         <article class="mb-5 border-bottom pb-3">
             <h2><a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a></h2>
