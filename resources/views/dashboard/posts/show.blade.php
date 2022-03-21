ae0c3aa73b18f40ac65fc499e669ad2ca1d4e8d3
tofatoff <mail.mstofa@gmail.com>
2022-03-21 10:50:45 +0700

    update and delete post

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -11,16 +11,18 @@
                     </span>
                     Back to my posts
                 </a>
-                <a href="#" class="btn btn-warning">
+                <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning">
                     <span data-feather="edit">
                     </span>
                     Edit
                 </a>
-                <a href="#" class="btn btn-danger">
-                    <span data-feather="x-circle">
-                    </span>
-                    Delete
-                </a>
+                <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                    @method('delete')
+                    @csrf
+                    <button class="btn btn-danger" onclick="confirm('Are you sure?')">
+                        <span data-feather="x-circle"></span>Delete
+                    </button>
+                </form>
 
                 <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="img-fluid mt-3"
                     alt="{{ $post->category->name }}">
